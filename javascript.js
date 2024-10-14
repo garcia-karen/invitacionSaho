6a9d872b82d2616fb0a01e378dffeb3eeda11a99
Karen Aidee <garcia.karen.170195@gmail.com>
2024-10-13 22:19:49 -0600

    Se agregan campos

---
+++ javascript.js
@@ -0,0 +1,32 @@
+function crearEnlace() {
+    var nombre = document.getElementById("nombre").value;
+    var asistencia = document.getElementById("asistencia").value;
+
+    var mensaje = "";
+
+    if (asistencia === "si") {
+      mensaje = "Hola, mi nombre es " + nombre + " y asistiré al evento. ¡Nos vemos allí!";
+    } else if (asistencia === "no") {
+      mensaje = "Hola, mi nombre es " + nombre + " y no podré asistir al evento. ¡Espero verlos la próxima vez!";
+    } else if (asistencia === "no estoy seguro") {
+      mensaje = "Hola, mi nombre es " + nombre + " y todavía no estoy seguro si podré asistir o no. ¡Hablemos pronto!";
+    }
+else if (asistencia === "ninguna") {
+      mensaje = "Hola, mi nombre es " + nombre + "...";
+    }
+
+    var telefono = "528118546934";
+    var url = "https://wa.me/" + telefono + "?text=" + encodeURIComponent(mensaje);
+
+    var boton = document.createElement("button");
+    boton.innerHTML = "Enviar mensaje a WhatsApp";
+    boton.addEventListener("click", function() {
+      window.open(url, "_blank");
+    });
+
+    var divBoton = document.getElementById("boton-whatsapp");
+    divBoton.innerHTML = "";
+    divBoton.appendChild(boton);
+  }
+
+  document.getElementById("asistencia").addEventListener("change", crearEnlace);
